43dbc4aed937b73163a4c3f0a387ebf677be7918
Calvin <46201774+calvinjlzhai@users.noreply.github.com>
2019-05-15 01:33:37 -0400

    Create movies.js
    Collecting data from user input group as object including movie name, rating and if had seen the movie previously in array. Displaying the output in the console.

---
+++ movies.js
@@ -0,0 +1,37 @@
+//input data into array filled with objects
+//listed the movie names, rating, and if had seen before or waiting to see it
+var movies = [
+    {
+        name: "Pacific Rims",
+        rating: 5,
+        hasWatched: true
+    },
+    {
+        name: "Life with Kai",
+        rating: 4,
+        hasWatched: false
+    },
+    {
+        name: "Pokemon",
+        rating: 5,
+        hasWatched: false
+    }
+]
+
+//loop over every object in the array
+movies.forEach(function(movie) {
+//Setting up the console output
+    var result = "You have ";
+//if person had seen the movie, output is watched is display
+    if(movie.hasWatched) {
+        result += "watched ";
+    } else {
+//if person did not seen the movie, output is display 
+        result += "not seen ";
+    }
+//putting the movie in quote marks and rating for the movie
+    result += "\"" + movie.name + "\" -"; 
+    result += movie.rating + " stars"
+//output
+    console.log(result);
+});
